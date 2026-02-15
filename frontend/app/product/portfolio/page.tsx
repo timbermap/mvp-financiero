a64ba4b9ed46f54120e9ce24865e217966823bac
snojek <36002391+snojek@users.noreply.github.com>
2026-02-15 14:24:43 -0300

    Versión inicial del MVP con backend y frontend

--- frontend/app/product/portfolio/page.tsx
+++ frontend/app/product/portfolio/page.tsx
@@ -1,2 +1,20 @@
-'use client';
+import React from "react";
 
+const PaginaEnDesarrollo: React.FC = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "2rem",
+        fontWeight: "bold",
+      }}
+    >
+      Página en desarrollo
+    </div>
+  );
+};
+
+export default PaginaEnDesarrollo;
